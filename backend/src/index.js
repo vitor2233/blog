67ca74120887753b5f7c5ce3df9fdfc0ed504307
vitor2233 <vitorguilherme2233@gmail.com>
2020-01-21 19:10:46 -0300

    Pastas e arquivos para backend

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
